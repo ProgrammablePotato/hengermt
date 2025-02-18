6f1e5ea451cb3fe27764091881d9362cda0c19d1
ProgrammablePotato <tamas.mihelik@gmail.com>
2025-02-18 10:39:19 +0100

    sok sok dolog

---
+++ src/main/java/com/example/MainController.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class MainController {
+
+    @FXML
+    private TextField heightField;
+
+    @FXML
+    private TextField radiusField;
+
+    @FXML
+    private TextField surfaceField;
+
+    @FXML
+    private ImageView gifView;
+
+    @FXML
+    void calculate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toAbout(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/example/Model.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Model {
+    public int id;
+    public Double radius;
+    public Double height;
+    public Double surface;
+}
